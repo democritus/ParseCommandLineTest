e3c13e461abb473e076ef308774d26cd9f9a9e42
Brian Warren <brian.scott.warren@gmail.com>
2021-10-15 13:13:04 -0500

    Removed unused vector.

--- CppConsole/CppConsole.cpp
+++ CppConsole/CppConsole.cpp
@@ -1,11 +1,9 @@
 #include <iostream>
-#include <vector>
 
 int main(int argc, char* argv[])
 {
     std::cout << "All args:\n";
     int i;
-    std::vector<std::string> v_args;
     for (i = 0; i < argc; ++i)
     {
         std::cout << i << "\t" << argv[i] << "\n";
